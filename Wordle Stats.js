6cfbd643e43d7f08e450f81843a150382b79dc31
anyhotcountry <anyhotcountry@users.noreply.github.com>
2024-12-03 09:06:56 +0000

    Output table with stats for each game

--- Wordle Stats.js
+++ Wordle Stats.js
@@ -6,17 +6,6 @@ const Logger = importModule("Logger.js");
 const { analyseWordle, getMedalSymbol, MEDALS, reverseSymbols } = importModule("wordle-module.js");
 const logger = new Logger();
 
-const uploadToExcel = async (data) => {
-  const url = 'https://prod-19.northeurope.logic.azure.com:443/workflows/65f2b4997aa647b085c3c8d1c2d5615c/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=hHVEPsNVSLNEpDsSrONfOF_yPiyawvyzQhg_VXM-meI';
-  const req = new Request(url);
-  req.method = "POST";
-  req.headers = {
-    "Content-Type": "application/json"
-  };
-  req.body = JSON.stringify(allResults);
-  await req.loadJSON();
-};
-
 const writeToFile = (data) => {
   const fm = FileManager.iCloud();
   const filePath = fm.joinPath(fm.bookmarkedPath('Pythonista 3'), 'wordle.json');
